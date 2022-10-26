8adfde67dbc5f1fb628e830d7d989971b2e78e70
srikanth.medisetti <srikanth.medisetti@CITHYDLTP-CRT07.CaprusIT.local>
2022-10-26 16:06:34 +0530

    Hi this is srikanth

---
+++ src/main/java/com/srikanth/SpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.srikanth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	System.out.println("........Success.............");
+	}
+
+}
